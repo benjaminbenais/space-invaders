0dee1da4e078daafe71df9f711ade76521286b34
benjamin benais <benjaminbenais@macbook-pro.home>
2020-07-07 12:36:46 +0200

    sound effects added

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
+// Canvas
 const canvas = document.querySelector('canvas');
+canvas.width = 600;
+canvas.height = 400;
 const ctx = canvas.getContext('2d');
 
+// Audio effects
+const shootAudio = new Audio('./assets/sound-effects/shoot.mp3');
+const playerExplosionAudio = new Audio('./assets/sound-effects/explosion.mp3');
+const invaderKilledAudio = new Audio(
+  './assets/sound-effects/invaderkilled.mp3'
+);
 // Helpers functions
 const randomIntInRange = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-canvas.width = 600;
-canvas.height = 400;
-
-// Game variables
-const FPS = 8;
+/* === GLOBAL VARIABLES === */
+// Game
+const FPS = 60;
 let keyDown = undefined; // Current key being pressed
-let fired = false; // Weither a shot has been fired
-let fire = false; // Weither trigger a shot or not
+const GREEN = '#00FF41';
 
-// Player variables
-const PLAYER_WIDTH = 30;
-const PLAYER_HEIGHT = 12;
-const PLAYER_DX = 1; // Player x velocity
-const PLAYER_COLOR = '#05dfd7';
-const canonPosition = {}; // Track the position of the canon (x & y) used to create missil
-// Player's missils variables
-const MISSIL_DY = 3; // Missil y velocity
-const MISSIL_COLOR = '#fff591';
+// Ship
+const SHIP_WIDTH = 30;
+const SHIP_HEIGHT = 12;
+const SHIP_DX = 0.5; // Ship x velocity
+const SHIP_COLOR = '#00FF41';
+const canonPosition = {}; // Track the position of the canon (x & y) used to create missile
 
-// Ennemies variables
+// Ship's missile
+const MISSILE_DY = 1; // y velocity
+const MISSILE_COLOR = '#00FF41';
+
+// Ennemies
 const ENNEMY_WIDTH = 20;
 const ENNEMY_HEIGHT = 12;
 const ENNEMIES_ROWS = 3;
 const ENNEMY_HEALTH = 1;
 
+// Obstacles
+const OBSTACLE_WIDTH = 100;
+const OBSTACLE_HEIGHT = 60;
+
 /* === CLASSES === */
-class Player {
+class Ship {
   constructor(x, y, width, height, dx) {
     this.x = x;
     this.y = y;
@@ -43,7 +54,7 @@ class Player {
 
   draw() {
     // Draw the base of the ship
-    ctx.fillStyle = PLAYER_COLOR;
+    ctx.fillStyle = SHIP_COLOR;
     ctx.fillRect(this.x, this.y, this.width, this.height);
     // Draw the canon of the ship
     const canonWidth = 6;
@@ -74,7 +85,7 @@ class Player {
   }
 }
 
-class Missil {
+class Missile {
   constructor(x, y, width, height, dy, id) {
     this.x = x;
     this.y = y;
@@ -84,12 +95,12 @@ class Missil {
     this.id = id;
   }
   draw() {
-    ctx.fillStyle = MISSIL_COLOR;
+    ctx.fillStyle = MISSILE_COLOR;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
     if (this.y <= -this.height) {
-      return (playerMissil = undefined);
+      return (shipMissile = undefined);
     }
     this.y -= this.dy;
     this.draw();
@@ -97,17 +108,16 @@ class Missil {
 }
 
 class Ennemy {
-  constructor(x, y, width, height, health, color) {
+  constructor(x, y, width, height, health) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.health = health;
-    this.color = color;
   }
 
   draw() {
-    ctx.fillStyle = this.color || '#ffffff';
+    ctx.fillStyle = '#ffffff';
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 
@@ -124,46 +134,58 @@ class Ennemy {
   }
 }
 
-let playerMissil;
+class Obstacle {
+  constructor(width, height, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+  }
 
-const createPlayerMissil = () => {
-  const newMissil = new Missil(
+  draw() {
+    ctx.fillRect;
+  }
+}
+
+let shipMissile;
+let ship;
+let ennemies = [];
+
+const createShipMissile = () => {
+  const newMissile = new Missile(
     canonPosition.x + 1,
     canonPosition.y,
     4,
-    4,
-    MISSIL_DY
+    8,
+    MISSILE_DY
   );
-  playerMissil = newMissil;
+  shootAudio.play();
+  shipMissile = newMissile;
 };
-
-let player;
-let ennemies = [];
 
 const createEnnemies = () => {
   let ennemyY = 60;
-  const colors = ['#00FF41', '#05dfd7', '#fa26a0']; // green, blue, purple
   const ennemiesPerRow = 11;
 
   for (let i = 0; i < ENNEMIES_ROWS; i++) {
     const row = [];
 
-    let countX = ENNEMY_WIDTH / 2;
+    const totalEnnemiesLength = ennemiesPerRow * ENNEMY_WIDTH * 2;
+    let ennemyX = (canvas.width - totalEnnemiesLength) / 2;
 
     for (let y = 0; y < ennemiesPerRow * 2; y++) {
-      const x = countX;
-      if (y % 2 !== 0) {
+      const x = ennemyX;
+      if (y % 2 === 0) {
         const ennemy = new Ennemy(
           x,
           ennemyY,
           ENNEMY_WIDTH,
           ENNEMY_HEIGHT,
-          ENNEMY_HEALTH,
-          colors[i]
+          ENNEMY_HEALTH
         );
         row.push(ennemy);
       }
-      countX += ENNEMY_WIDTH;
+      ennemyX += ENNEMY_WIDTH;
     }
 
     ennemies.push(row);
@@ -172,13 +194,13 @@ const createEnnemies = () => {
 };
 
 const init = () => {
-  // Create player instance
-  player = new Player(
-    (canvas.width - PLAYER_WIDTH) / 2, // X position (middle of the canvas)
-    canvas.height - PLAYER_HEIGHT - 20, // Y position (20px above bottom of canvas)
-    PLAYER_WIDTH,
-    PLAYER_HEIGHT,
-    PLAYER_DX // Player x velocity
+  // Create ship instance
+  ship = new Ship(
+    (canvas.width - SHIP_WIDTH) / 2, // X position (middle of the canvas)
+    canvas.height - SHIP_HEIGHT - 20, // Y position (20px above bottom of canvas)
+    SHIP_WIDTH,
+    SHIP_HEIGHT,
+    SHIP_DX // Ship x velocity
   );
 
   createEnnemies();
@@ -187,9 +209,9 @@ const init = () => {
 const loop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Update player missil position when created
-  if (playerMissil) {
-    playerMissil.update();
+  // Update ship missile position when created
+  if (shipMissile) {
+    shipMissile.update();
   }
 
   if (ennemies && ennemies.length) {
@@ -198,14 +220,19 @@ const loop = () => {
         for (let y = 0; y < ennemies[i].length; y++) {
           const currentEnnemy = ennemies[i][y];
           if (
-            playerMissil &&
+            shipMissile &&
             currentEnnemy.health &&
-            playerMissil.y <= currentEnnemy.y + currentEnnemy.height &&
-            playerMissil.x >= currentEnnemy.x &&
-            playerMissil.x <= currentEnnemy.x + currentEnnemy.width
+            shipMissile.y + MISSILE_DY <
+              currentEnnemy.y + currentEnnemy.height &&
+            shipMissile.x >= currentEnnemy.x &&
+            shipMissile.x <= currentEnnemy.x + currentEnnemy.width
           ) {
             currentEnnemy.decreaseHealth();
-            playerMissil = undefined;
+
+            if (currentEnnemy.health === 0) {
+              invaderKilledAudio.play();
+            }
+            shipMissile = undefined;
           }
           currentEnnemy.update();
         }
@@ -215,35 +242,32 @@ const loop = () => {
 
   // Check if key is pressed
   if (keyDown === 'left') {
-    player.moveLeft();
+    ship.moveLeft();
   }
 
   if (keyDown === 'right') {
-    player.moveRight();
+    ship.moveRight();
   }
 
-  if (!fired) {
-    if (fire && !playerMissil) {
-      fired = true;
-      createPlayerMissil();
-    }
-  }
-
-  player.update();
+  ship.update();
 };
 
 const keyDownHandler = e => {
+  // Left key
   if (e.keyCode === 37 || e.which === 37) {
     keyDown = 'left';
   }
 
+  // Right key
   if (e.keyCode === 39 || e.which === 39) {
     keyDown = 'right';
   }
 
+  // Space key
   if (e.keyCode === 32 || e.which === 32) {
-    if (!fired) {
-      fire = true;
+    // If no missile currently running, create one
+    if (!shipMissile) {
+      createShipMissile();
     }
   }
 };
@@ -254,11 +278,6 @@ const keyUpHandler = e => {
   }
   if (e.keyCode === 39 || e.which === 39) {
     keyDown = undefined;
-  }
-
-  if (e.keyCode === 32 || e.which === 32) {
-    fired = false;
-    fire = false;
   }
 };
 
